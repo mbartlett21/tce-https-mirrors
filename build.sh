4af0cec09683cea4a6660f74364f540fe3bc3dc4
mbartlett21 <mjmouse9999@gmail.com>
2026-01-22 10:58:42 +1000

    Full

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
+#!/bin/sh
 tce-load -wil squashfs-tools
+
+rm https-mirrors.tcz*
+
 mksquashfs https-mirrors https-mirrors.tcz
+
+echo openssl.tcz >>https-mirrors.tcz.dep
+echo ca-certificates.tcz >>https-mirrors.tcz.dep
+
+if [ "$1" == "install" ]; then
+	cp https-mirrors.tcz* -t /etc/sysconfig/tcedir/optional
+	echo https-mirrors.tcz >>/etc/sysconfig/tcedir/onboot.lst
+	tce-load -i https-mirrors.tcz
+fi
