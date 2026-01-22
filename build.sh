5633a3e7c35ad9f6fcb05294901b2c7ca202df35
mbartlett21 <mjmouse9999@gmail.com>
2026-01-22 11:38:57 +1000

    Update

--- build.sh
+++ build.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 tce-load -wil squashfs-tools
 
-rm https-mirrors.tcz*
+rm -rf build
 sudo rm -rf /tmp/https-mirrors
+
+mkdir build
 
 sudo mkdir -p /tmp/https-mirrors/usr/local/https-mirrors
 sudo mkdir -p /tmp/https-mirrors/usr/local/tce.installed
+sudo mkdir -p /tmp/https-mirrors/usr/local/share/doc/https-mirrors
 
+sudo cp COPYING                 /tmp/https-mirrors/usr/local/share/doc/https-mirrors/COPYING
 sudo cp https-mirrors.patch     /tmp/https-mirrors/usr/local/https-mirrors/https-mirrors.patch
 sudo cp https-mirrors.installed /tmp/https-mirrors/usr/local/tce.installed/https-mirrors
 
-mksquashfs /tmp/https-mirrors https-mirrors.tcz
+mksquashfs /tmp/https-mirrors build/https-mirrors.tcz
 
-echo openssl.tcz >>https-mirrors.tcz.dep
-echo ca-certificates.tcz >>https-mirrors.tcz.dep
+echo openssl.tcz >>build/https-mirrors.tcz.dep
+echo ca-certificates.tcz >>build/https-mirrors.tcz.dep
 
 if [ "$1" == "install" ]; then
-	cp https-mirrors.tcz* -t /etc/sysconfig/tcedir/optional
+	cp build/https-mirrors.tcz* -t /etc/sysconfig/tcedir/optional
 	echo https-mirrors.tcz >>/etc/sysconfig/tcedir/onboot.lst
 	tce-load -i https-mirrors.tcz
 fi
